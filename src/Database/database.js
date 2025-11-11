3ebfe5bbb3630c3b0bedbb7e2f5d439f39c909d7
pavan90-one <88099058+pavan90-one@users.noreply.github.com>
2025-11-11 19:50:56 +0530

    Delete src directory

--- src/Database/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const config = require('../Config/index');
-
-const connectDB = async()=>{
-   await mongoose.connect(config.mongoUri);       
-}
-
-module.exports = connectDB
--- src/Healper/jwttoken.js
+++
@@ -1,8 +0,0 @@
-const { signAccessToken, signRefreshToken, verifyRefreshToken } = require('../Utilities/jwt');
-exports.createTokensForUser = (user) => {
-  // include tokenVersion so we can revoke refresh tokens by bumping tokenVersion in DB
-  const payload = { userId: user._id, role: user.doctorRole };
-   const access = signAccessToken(payload);
-  const refresh = signRefreshToken({ ...payload, tokenVersion: user.tokenVersion });
-  return { access, refresh };
-};
