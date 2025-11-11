b11abcfad2d71d50178f111ab2df30372e4e506c
pavan90-one <88099058+pavan90-one@users.noreply.github.com>
2025-11-11 19:53:48 +0530

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-// jest.config.js
-module.exports = {
-  testEnvironment: 'node', // Use the Node.js environment for testing
-  testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'], // Define patterns for test files
-  // You can add more configurations here, like coverage thresholds, module file extensions, etc.
-};
