f8eed20ea00d756f9e848adc33f9ea33cb5b2885
pawan <apavanreddy1003@gmail.com>
2025-11-11 18:56:46 +0530

    database changes

--- src/Database/database.js
+++ src/Database/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const config = require('../Config/index');
 
 const connectDB = async()=>{
-   await mongoose.connect(config.mongoUri);       
+   await mongoose.connect("mongodb://localhost:27017/healthGoals");       
 }
 
 module.exports = connectDB
